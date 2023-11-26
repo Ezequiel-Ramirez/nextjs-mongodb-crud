b868b34965ac6f073e488637362d509788f59823
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2023-11-26 19:01:20 -0300

    Add TaskCard component and display tasks on HomePage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,18 @@
+import TaskCard from '@/components/TaskCard'
+import Task from '@/models/Task'
 
-const HomePage = () => {
+const HomePage = async () => {
+  const tasks = await Task.find()
+  
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>Tasks</h1>
+      <div className='grid grid-cols-3 gap-2'>
+        {tasks.map((task) => (
+          <TaskCard task={task}  key={task._id}/>
+        ))}
+      </div>
+    </div>
   )
 }
 
